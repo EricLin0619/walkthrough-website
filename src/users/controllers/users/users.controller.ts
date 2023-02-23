e56036e478d49255a7ac00e8e21a6aa0e79ed345
Eric Lin <40213852eric@gmail.com>
2023-02-23 11:35:21 +0800

    finish middleware

---
+++ src/users/controllers/users/users.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseBoolPipe,
+  ParseIntPipe,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UsersService } from '../../services/users/users.service';
+import { CreateUserDto } from '../../dtos/CreateUser.dto';
+
+@Controller('users') // api or route
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Get('query') // decorators are basically just functions
+  getUsers(@Query('sortDesc', ParseBoolPipe) sortDesc: boolean) {
+    console.log(sortDesc);
+    return { username: 'Eric', email: 'eric@eric.com' };
+  }
+
+  @Post('create')
+  @UsePipes(new ValidationPipe())
+  createUser(@Body() userData: CreateUserDto) {
+    console.log(userData.email);
+    console.log(userData.username);
+    this.userService.createUser(userData);
+    return { ...userData };
+  }
+
+  @Get(':id')
+  getUserById(@Param('id', ParseIntPipe) id: number) {
+    const user = this.userService.fetchUserById(id);
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
+    }
+    return user;
+  }
+
+  @Get()
+  getUserByService() {
+    console.log('controller');
+    return this.userService.fetchUsers();
+  }
+}
